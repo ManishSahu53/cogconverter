28101b6370e1d29d31515579825409ee13e71bf3
Manish Sahu <manishsahu@Manishs-Air.Dlink>
2019-06-11 19:17:26 +0530

    removed version loading

--- cogconverter/__init__.py
+++ cogconverter/__init__.py
@@ -12,7 +12,3 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging_config.get_console_handler())
 logger.propagate = False
-
-
-with open('./VERSION') as version_file:
-    __version__=version_file.read().strip()
